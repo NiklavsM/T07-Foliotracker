1b7d5087d1c8b2daa89640dbaf8a51e40427bb43
Luca <luca.demelas.2015@uni.strath.ac.uk>
2017-11-09 15:36:20 +0000

    started building a GUI

---
+++ src/MainFrame.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+
+    public void mainFrame() {
+        JPanel panel = new JPanel();
+        JButton button = new JButton("Add");
+        JLabel label1 = new JLabel("Ticketer Symbol:");
+        JLabel label2 = new JLabel("Number of Shares:");
+        TextField tf1 = new TextField("",20);
+        TextField tf2 = new TextField("",20);
+        this.add(panel);
+        panel.add(button);
+        panel.add(label1);
+        panel.add(tf1);
+        panel.add(label2);
+        panel.add(tf2);
+        this.add(panel);
+        this.setSize(800,300);
+        this.setVisible(true);
+    }
+
+
+    public static void main(String[] args){
+        MainFrame mainframe = new MainFrame();
+        mainframe.mainFrame();
+    }
+
+
+}
